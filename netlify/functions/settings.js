dcb6d9b4847bb8ceb12b304bb264fc08b3d6e1fc
keithys2r <keithytrades@gmail.com>
2026-08-17 08:37:25 -0400

    Update settings.js

--- netlify/functions/settings.js
+++ netlify/functions/settings.js
@@ -6,7 +6,7 @@
 
 const memory = require('./memory');
 
-const ALLOWED_SYMBOLS = ['R_10', 'R_25', 'R_50', 'R_75', 'R_100', '1HZ10V', '1HZ25V', '1HZ50V', '1HZ75V', '1HZ100V'];
+const ALLOWED_SYMBOLS = ['R_10', 'R_25', 'R_50', 'R_75', 'R_100', '1HZ10V', '1HZ25V', '1HZ50V', '1HZ75V', '1HZ100V', 'frxXAUUSD'];
 
 exports.handler = async function (event) {
   if (event.httpMethod === 'OPTIONS') {
